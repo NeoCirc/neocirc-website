b693c4177649d4a699dd01c6e24aa3ac123fe341
Masasasaki <masakiasanuma@outlook.com>
2021-11-15 18:27:11 -0500

    Initial setup of react application

---
+++ src/components/SideBar.js
@@ -0,0 +1,11 @@
+import { ProSidebar, Menu, MenuItem, SubMenu, SidebarHeader, SidebarFooter, SidebarContent } from 'react-pro-sidebar';
+import { FaHeart, FaGem } from 'react-icons/fa'
+import 'react-pro-sidebar/dist/css/styles.css';
+
+const SideBar = () => {
+    return (<div>
+        
+    </div>);
+}
+
+export { SideBar };
