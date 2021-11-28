aef5308058850a791c9564d04f48b9041727bd26
Masasasaki <masakiasanuma@outlook.com>
2021-11-28 14:42:17 -0500

    Quick fix module quiz page alignment

--- src/components/modules/quizzes/ModuleQuizPage.js
+++ src/components/modules/quizzes/ModuleQuizPage.js
@@ -6,14 +6,18 @@ const ModuleQuizPage = () => {
             <fieldset>
                 <Form.Group as={Row} className="mb-3">
                     <Col>
-                        <Form.Label>Q1.  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis nec diam urna?</Form.Label>
-                        <Image
-                            src="https://ca-times.brightspotcdn.com/dims4/default/5010365/2147483647/strip/true/crop/600x405+0+0/resize/840x567!/format/webp/quality/90/?url=https%3A%2F%2Fcalifornia-times-brightspot.s3.amazonaws.com%2Fba%2F6f%2F86f3134a8c819e0925a7df13e153%2Fla-xpm-photo-2013-aug-22-la-sci-sn-circumcision-us-rates-cdc-20130822"
-                            fluid
-                            style={{
-                                maxWidth: '750px',
-                            }}
-                        />
+                        <Col>
+                            <Form.Label>Q1.  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis nec diam urna?</Form.Label>
+                        </Col>
+                        <Col>
+                            <Image
+                                src="https://ca-times.brightspotcdn.com/dims4/default/5010365/2147483647/strip/true/crop/600x405+0+0/resize/840x567!/format/webp/quality/90/?url=https%3A%2F%2Fcalifornia-times-brightspot.s3.amazonaws.com%2Fba%2F6f%2F86f3134a8c819e0925a7df13e153%2Fla-xpm-photo-2013-aug-22-la-sci-sn-circumcision-us-rates-cdc-20130822"
+                                fluid
+                                style={{
+                                    maxWidth: '750px',
+                                }}
+                            />
+                        </Col>
                         <Col>
                             <Form.Check
                                 type="radio"
