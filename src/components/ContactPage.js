34fda6f3e51dc9f2a1e95e0a5dcfc9c84e07c53f
Masasasaki <masakiasanuma@outlook.com>
2022-01-26 12:44:16 -0500

    Change styling for contact page

--- src/components/ContactPage.js
+++ src/components/ContactPage.js
@@ -9,17 +9,17 @@ const ContactPage = () => {
                 Contact Us!
             </Heading>
             <ContactForm>
-                <Form.Group className="mb-3">
+                <Form.Group className="mb-4">
                     <ContactLabel>Name</ContactLabel>
                     <Form.Control type="name" placeholder="Enter Name" />
                 </Form.Group>
 
-                <Form.Group className="mb-3">
+                <Form.Group className="mb-4">
                     <ContactLabel>Email</ContactLabel>
                     <Form.Control type="email" placeholder="Enter Email" />
                 </Form.Group>
 
-                <Form.Group className="mb-3">
+                <Form.Group className="mb-4">
                     <ContactLabel>Message</ContactLabel>
                     <Form.Control as="textarea" rows={5} type="message" placeholder="Enter Message"/>
                 </Form.Group>
@@ -33,7 +33,8 @@ const ContactPage = () => {
 };
 
 const ContactLabel = styled(Form.Label)`
-    font-weight: bolder;
+    font-weight: light;
+    font-size: 25px;
 `;
 
 const SendMessageButton = styled(Button)`
@@ -41,21 +42,28 @@ const SendMessageButton = styled(Button)`
 `;
 
 const Wrap = styled.div`
-    width: 90vw;
+    width: 50vw;
     display: flex;
     justify-content: center;
     align-items: center;
     flex-direction: column;
+    background: linear-gradient(to bottom, #99ccff 0%, #ffccff 100%);
+    padding: 30px 0px 30px 0px;
+    border-radius: 30px;
+    margin-left: 35%;
 `;
 
 const ContactForm = styled(Form)`
-    min-width: 500px;
+    display: flex;
+    flex-direction: column;
+    width: 75%;
 `;
 
 const Heading = styled.h1`
     font-weight: lighter;
-    font-size: 70px;
+    font-size: 4em;
     text-align: center;
+    padding-bottom: 20px;
 `;
 
 export { ContactPage };
