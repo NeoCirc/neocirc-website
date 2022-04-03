3798abbaf500b7cf2e314db3fb978a676b0cb8ca
Masasasaki <masakiasanuma@outlook.com>
2022-04-03 17:18:24 -0400

    Remove team bio section of about us and implement mobile scalability

--- src/components/AboutUsPage.js
+++ src/components/AboutUsPage.js
@@ -1,6 +1,4 @@
 import styled from 'styled-components';
-import Card from 'react-bootstrap/Card';
-import samplePortrait from './component-images/sample-portrait.png';
 
 
 const AboutUsPage = () => {
@@ -19,119 +17,6 @@ const AboutUsPage = () => {
                 Our product will strive to become a unifying resource in the marketplace for neonatal circumcision to prevent unexpected
                 complications and burdens of involving parties.
             </ParagraphWrap>
-
-            <SubHeading>
-                Meet Our Team
-            </SubHeading>
-
-            <PortraitWrap>
-
-                <SubHeading2 width="95%">
-                    NeoCirc inc.
-                </SubHeading2>
-
-                <ParagraphWrap width="100%" margin="0% 2.5% 1.5em">
-                    Description
-                </ParagraphWrap>
-
-                <Card style={{ width: '20%', margin: '2.5%', display: 'inline-block' }}>
-                    <Card.Img variant="top" src={samplePortrait} />
-                    <Card.Body>
-                        <Card.Title>Emily Blum</Card.Title>
-                        <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
-                        </Card.Text>
-                    </Card.Body>
-                </Card>
-
-                <Card style={{ width: '20%', margin: '2.5%', display: 'inline-block' }}>
-                    <Card.Img variant="top" src={samplePortrait} />
-                    <Card.Body>
-                        <Card.Title>Emily Blum</Card.Title>
-                        <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
-                        </Card.Text>
-                    </Card.Body>
-                </Card>
-
-                <Card style={{ width: '20%', margin: '2.5%', display: 'inline-block' }}>
-                    <Card.Img variant="top" src={samplePortrait} />
-                    <Card.Body>
-                        <Card.Title>Emily Blum</Card.Title>
-                        <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
-                        </Card.Text>
-                    </Card.Body>
-                </Card>
-
-                <SubHeading2 width="95%">
-                    Development Team
-                </SubHeading2>
-
-                <ParagraphWrap width="100%" margin="0% 2.5% 1.5em">
-                    Description
-                </ParagraphWrap>
-
-                <Card style={{ width: '20%', margin: '2.5%', display: 'inline-block' }}>
-                    <Card.Img variant="top" src={samplePortrait} />
-                    <Card.Body>
-                        <Card.Title>Valentin Stelea</Card.Title>
-                        <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
-                        </Card.Text>
-                    </Card.Body>
-                </Card>
-
-                <Card style={{ width: '20%', margin: '2.5%', display: 'inline-block' }}>
-                    <Card.Img variant="top" src={samplePortrait} />
-                    <Card.Body>
-                        <Card.Title>Sebastian Wilson</Card.Title>
-                        <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
-                        </Card.Text>
-                    </Card.Body>
-                </Card>
-
-                <Card style={{ width: '20%', margin: '2.5%', display: 'inline-block' }}>
-                    <Card.Img variant="top" src={samplePortrait} />
-                    <Card.Body>
-                        <Card.Title>Khelan Modi</Card.Title>
-                        <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
-                        </Card.Text>
-                    </Card.Body>
-                </Card>
-
-                <Card style={{ width: '20%', margin: '2.5%', display: 'inline-block' }}>
-                    <Card.Img variant="top" src={samplePortrait} />
-                    <Card.Body>
-                        <Card.Title>Masaki Asanuma</Card.Title>
-                        <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
-                        </Card.Text>
-                    </Card.Body>
-                </Card>
-
-                <Card style={{ width: '20%', margin: '2.5%', display: 'inline-block' }}>
-                    <Card.Img variant="top" src={samplePortrait} />
-                    <Card.Body>
-                        <Card.Title>Kevin Quayle</Card.Title>
-                        <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
-                        </Card.Text>
-                    </Card.Body>
-                </Card>
-            
-             </PortraitWrap>
-
         </Wrap>
     )
 };
@@ -140,6 +25,9 @@ const AboutUsPage = () => {
 const Wrap = styled.div`
     background: linear-gradient(to bottom, #99ccff 0%, #ffccff 100%);
     margin: 0px 50px 50px 50px;
+    @media (max-width: 768px) {
+        margin: 0px;
+    }
 `;
 
 const ParagraphWrap = styled.span`
@@ -151,34 +39,12 @@ const ParagraphWrap = styled.span`
     text-align: justify;
 `;
 
-const PortraitWrap = styled.div`
-    padding-bottom: 200px;
-`;
-
 const Heading = styled.h1`
     font-weight: lighter;
     padding-top: 1em;
     padding-bottom: 1em;
     font-size:70px;
     text-align: center;
-`;
-
-const SubHeading = styled.h2`
-    font-weight: lighter;
-    padding-top: 2em;
-    padding-bottom: 1em;
-    font-size: 60px;
-    text-align: center;
-`;
-
-const SubHeading2 = styled.h3`
-    width: ${props => props.width};
-    margin: .2em 2.5%;
-    font-weight: bolder;
-    padding-top: .5em;
-    padding-bottom: 0em;
-    font-size: 40px;
-    text-align: left;
 `;
 
 export { AboutUsPage };
